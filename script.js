ca143767e40b7dbbcc92d04ee01434ef631930ee
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-03-13 03:17:47 +0530

    matrix effect

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-addEventListener ("mousemove" , (e) => {
-    console.log(e.clientX, e.clientY);
+// addEventListener ("mousemove" , (e) => {
+//     console.log(e.clientX, e.clientY);
 
-    document.body.style.setProperty("--x", e.clientX + "px");
-    document.body.style.setProperty("--y", e.clientY + "px");
+//     document.body.style.setProperty("--x", e.clientX + "px");
+//     document.body.style.setProperty("--y", e.clientY + "px");
+// })
+
+const para = document.querySelector("p")
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const text = para.innerText
+
+para.addEventListener("mouseenter", () => {
+
+
+    setInterval(() => {
+
+        const str = text.split('').map((char, index) => {
+            return characters.split("")[ Math.floor(Math.random() * 53) ]
+        }).join("")
+
+        para.innerText = str
+
+    }, 30)
+
+
+
 })
